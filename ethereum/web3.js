1501b9b7872c0f79301fc77ce37a3d87c6d05977
Shashank H R <107236671+shankhr@users.noreply.github.com>
2022-09-30 15:22:19 +0530

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/e059bbdcb06043809cf5d03303bc5f1e"
+    "infura link"
   );
   web3 = new Web3(provider);
 }
